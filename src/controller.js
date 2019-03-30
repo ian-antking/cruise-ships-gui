729395eae4909bc8f01cc77aef65a39577a2154d
Richard Pentecost <richardpentecost1@gmail.com>
2019-03-30 18:34:29 +0000

    ports done

--- src/controller.js
+++ src/controller.js
@@ -9,10 +9,23 @@ class Controller {
       './images/water1.png',
     ];
     let backgroundIndex = 0;
-     
     window.setInterval(() => {
       document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
       backgroundIndex += 1;
     }, 1000);
+  }
+
+  renderPorts(ports) {
+    const portsElement = document.querySelector('#ports');
+    portsElement.style.width = '0px';
+    ports.forEach((port, index) => {
+      const newPortElement = document.createElement('div');
+      newPortElement.className = 'port';
+      newPortElement.dataset.portIndex = index;
+      newPortElement.dataset.portName = port.name;
+      portsElement.appendChild(newPortElement);
+      const portsElementWidth = parseInt(portsElement.style.width, 10);
+      portsElement.style.width = `${portsElementWidth + 256}px`;
+    });
   }
 }
--- src/port.js
+++ src/port.js
@@ -1,20 +1,26 @@
-class Port {
-  constructor(name) {
-    this.name = name;
-    this.ships = [];
-  }
-
-  addShip(ship) {
-    this.ships.push(ship);
-  }
-
-  removeShip(ship) {
-    if (!this.ships.includes(ship)) {
-      throw new Error('ship is not docked');
+(function exportPort() {
+  class Port {
+    constructor(name) {
+      this.name = name;
+      this.ships = [];
     }
-    const shipIndex = this.ships.indexOf(ship);
-    this.ships.splice(shipIndex, 1);
-  }
-}
 
-module.exports = Port;
+    addShip(ship) {
+      this.ships.push(ship);
+    }
+
+    removeShip(ship) {
+      if (!this.ships.includes(ship)) {
+        throw new Error('ship is not docked');
+      }
+      const shipIndex = this.ships.indexOf(ship);
+      this.ships.splice(shipIndex, 1);
+    }
+  }
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
+  }
+}());
